50c3d95c584b2cfb542b68c83c0c0646b97c1e82
James Giosmas <giosmasj@gmail.com>
2020-07-31 08:23:05 -0600

    Completed Return Negative Kata for Java 8kyu on 7/31/2020, my solution which is ranked #2 and best practice included. Clever to use -abs(x)

--- Java-Codewars.java
+++ Java-Codewars.java
@@ -1,5 +1,7 @@
 // We need a function that can transform a number into a string
 // What ways of achieving this do you know?
+// 8kyu
+// 7/30/2020
 // Examples:
 Kata.numberToString(123) // returns "123"
 Kata.numberToString(999) // returns "999"
@@ -14,4 +16,35 @@ class Kata {
   public static String numberToString(int num) {
     return String.valueOf(num);
   }
+}
+
+// In this simple assignment you are give a number and have to make it
+// negative. But maybe the number is already negative?
+// Example:
+Kata.makeNegative(1) // returns -1
+Kata.makeNegative(-5) // returns -5
+Kata.makeNegative(0) // returns 0
+// 8kyu
+// 7/31/2020
+
+public class Kata {
+
+  public static int makeNegative(final int x) {
+
+    return (x < 0) ? x : -x;
+
+  }
+
+}
+
+// best practice
+
+import static java.lang.Math.abs;
+
+public class Kata {
+
+  public static int makeNegative(final int x) {
+    return -abs(x);
+  }
+  
 }
