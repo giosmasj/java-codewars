abf007b3ecfea54bc4b35e08ab839a87fa0fe7cc
James Giosmas <giosmasj@gmail.com>
2020-08-04 11:33:08 -0600

    Vowels Count Kata, in Java. My solution and best practice. Idk if I would've ever gone the route of best practice, but seeing all the solutions is always cool

--- Java-Codewars.java
+++ Java-Codewars.java
@@ -48,3 +48,34 @@ public class Kata {
   }
   
 }
+
+// Vowels Count Kata
+// 7kyu
+// 8/4/2020
+// Return the number (count) of vowels in the given string.
+// We will consider a, e, i, o, u as vowels but not y.
+// The input string will only consist of lower case letters and/or spaces.
+
+public class Vowels {
+
+  public static int getCount(String str) {
+    int vowelsCount = 0;
+    for (int i = 0; i < str.length(); i++){
+      if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u'){
+        vowelsCount++;
+      }
+    }
+    vowelsCount;
+  }
+
+}
+
+// Best Practice, very clever but don't know if I would've gone that route
+
+public class Vowels {
+
+  public static int getCount(String str) {
+    return str.replaceAll("(?i)[^aeiou]", "").length();
+  }
+
+}
