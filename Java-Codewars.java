d9814a7ad03dfe4d589b788d984e333c11f6f6df
James Giosmas <giosmasj@gmail.com>
2020-09-18 18:07:55 -0600

    completed String repeat kata 8kyu, love looking at the first few best practice solutions

--- Java-Codewars.java
+++ Java-Codewars.java
@@ -3,51 +3,51 @@
 // 8kyu
 // 7/30/2020
 // Examples:
-Kata.numberToString(123) // returns "123"
-Kata.numberToString(999) // returns "999"
+// Kata.numberToString(123) // returns "123"
+// Kata.numberToString(999) // returns "999"
 
-class Kata {
-  public static String numberToString(int num) {
-    return Integer.toString(num);
-  }
-}
+// class Kata {
+//   public static String numberToString(int num) {
+//     return Integer.toString(num);
+//   }
+// }
 // best practice
-class Kata {
-  public static String numberToString(int num) {
-    return String.valueOf(num);
-  }
-}
+// class Kata {
+//   public static String numberToString(int num) {
+//     return String.valueOf(num);
+//   }
+// }
 
 // In this simple assignment you are give a number and have to make it
 // negative. But maybe the number is already negative?
 // Example:
-Kata.makeNegative(1) // returns -1
-Kata.makeNegative(-5) // returns -5
-Kata.makeNegative(0) // returns 0
+// Kata.makeNegative(1) // returns -1
+// Kata.makeNegative(-5) // returns -5
+// Kata.makeNegative(0) // returns 0
 // 8kyu
 // 7/31/2020
 
-public class Kata {
+// public class Kata {
 
-  public static int makeNegative(final int x) {
+//   public static int makeNegative(final int x) {
 
-    return (x < 0) ? x : -x;
+//     return (x < 0) ? x : -x;
 
-  }
+//   }
 
-}
+// }
 
 // best practice
 
-import static java.lang.Math.abs;
+// import static java.lang.Math.abs;
 
-public class Kata {
+// public class Kata {
 
-  public static int makeNegative(final int x) {
-    return -abs(x);
-  }
+//   public static int makeNegative(final int x) {
+//     return -abs(x);
+//   }
   
-}
+// }
 
 // Vowels Count Kata
 // 7kyu
@@ -56,26 +56,41 @@ public class Kata {
 // We will consider a, e, i, o, u as vowels but not y.
 // The input string will only consist of lower case letters and/or spaces.
 
-public class Vowels {
+// public class Vowels {
 
-  public static int getCount(String str) {
-    int vowelsCount = 0;
-    for (int i = 0; i < str.length(); i++){
-      if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u'){
-        vowelsCount++;
-      }
-    }
-    vowelsCount;
-  }
+//   public static int getCount(String str) {
+//     int vowelsCount = 0;
+//     for (int i = 0; i < str.length(); i++){
+//       if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u'){
+//         vowelsCount++;
+//       }
+//     }
+//     vowelsCount;
+//   }
 
-}
+// }
 
 // Best Practice, very clever but don't know if I would've gone that route
 
-public class Vowels {
+// public class Vowels {
 
-  public static int getCount(String str) {
-    return str.replaceAll("(?i)[^aeiou]", "").length();
+//   public static int getCount(String str) {
+//     return str.replaceAll("(?i)[^aeiou]", "").length();
+//   }
+
+// }
+
+// Write a function called repeat_str which repeats the given string src exactly count times.
+
+// repeatStr(6, "I") // "IIIIII"
+// repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+public class Solution {
+  public static String repeatStr(final int repeat, final String string) {
+    String repeatedString = "";
+    for (int i = 0; i < repeat; i++){
+      repeatedString += string;
+    }
+      return repeatedString;
   }
-
 }
