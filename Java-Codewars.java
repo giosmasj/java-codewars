876d46b0a4e4a64bd15e95386e1d8041c6fe0db0
James Giosmas <giosmasj@gmail.com>
2020-09-25 12:35:42 -0600

    Printer Error Kata 7kyu in Java, had to use a lifeline on this one and learned you cannot use == to compare a string but you can use .equals method. Love learning new things all the time

--- Java-Codewars.java
+++ Java-Codewars.java
@@ -166,29 +166,78 @@
 
 // HackerRank Java Loops II Solution:
 
-import java.util.*;
-import java.io.*;
+// import java.util.*;
+// import java.io.*;
 
-class Solution{
-    public static void main(String []argh){
-        Scanner in = new Scanner(System.in);
-        int t=in.nextInt();
-        for(int i=0;i<t;i++){
-            int a = in.nextInt();
-            int b = in.nextInt();
-            int n = in.nextInt();
+// class Solution{
+//     public static void main(String []argh){
+//         Scanner in = new Scanner(System.in);
+//         int t=in.nextInt();
+//         for(int i=0;i<t;i++){
+//             int a = in.nextInt();
+//             int b = in.nextInt();
+//             int n = in.nextInt();
 
-            for(int j=0; j<n; j++){
-                a += b;
+//             for(int j=0; j<n; j++){
+//                 a += b;
 
-                if (j>0){
-                    System.out.print(" ");
-                }
-                System.out.print(a);
-                b = b*2;
-            }
-            System.out.println(" ");
+//                 if (j>0){
+//                     System.out.print(" ");
+//                 }
+//                 System.out.print(a);
+//                 b = b*2;
+//             }
+//             System.out.println(" ");
+//         }
+//         in.close();
+//     }
+// }
+
+// In a factory a printer prints labels for boxes. For one kind of boxes
+// the printer has to use colors which, for the sake of simplicity, are
+// named with letters from a to m.
+
+// The colors used by the printer are recorded in a control string. For
+// example a "good" control string would be aaabbbbhaijjjm meaning that
+// the printer used three times color a, four times color b, one time
+// color h then one time color a...
+
+// Sometimes there are problems: lack of colors, technical malfunction
+// and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm
+// with letters not from a to m.
+
+// You have to write a function printer_error which given a string will
+// return the error rate of the printer as a string representing a
+// rational whose numerator is the number of errors and the denominator
+// the length of the control string. Don't reduce this fraction to a
+// simpler expression.
+
+// The string has a length greater or equal to one and contains only
+// letters from ato z.
+
+// Examples:
+// s="aaabbbbhaijjjm"
+// error_printer(s) => "0/14"
+
+// s="aaaxbbbbyyhwawiwjjjwwm"
+// error_printer(s) => "8/22"
+
+public class Printer {
+
+  public static String printerError(String s) {
+    int count = 0;
+    String[] arr = s.split("");
+    int length = s.length();
+    String errors = "nopqrstuvwxyz";
+    String[] error_array = errors.split("");
+    int error_array_length = errors.length();
+    for (int i=0; i<length; i++){
+      for (int j=0; j<error_array_length; j++){
+        if (arr[i].equals(error_array[j])){
+          count += 1;
         }
-        in.close();
+      }
     }
+    return count + "/" + length;
+  }
 }
